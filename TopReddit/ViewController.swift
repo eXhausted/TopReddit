c7482192d39dc5b94eed7ce8e49fb5e40de890cd
Bogdan Geleta <bogdan.geleta@gmail.com>
2020-12-11 10:37:25 +0200

    Initial Commit

---
+++ TopReddit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TopReddit
+//
+//  Created by Bogdan Geleta on 11.12.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
